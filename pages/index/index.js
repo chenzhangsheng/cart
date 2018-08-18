5945e09b11622413e5bb9559761bd635826a24bc
‘chenzhangsheng’ <690619782@qq.com>
2018-08-19 00:33:10 +0800

    version-0.1

---
+++ pages/index/index.js
@@ -0,0 +1,87 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    motto: 'Hello World',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad: function () {
+    console.log(wx)
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse){
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+   
+  },
+  onReady: function (){
+    console.log("test1 onReady");
+  
+        const ctx = wx.createCanvasContext('shareCanvas')
+        // 底图
+        ctx.drawImage('../../images/backg.jpg', 0, 0, 600, 900)
+        // 作者名称
+        ctx.setTextAlign('center')    // 文字居中
+        ctx.setFillStyle('#000000')  // 文字颜色：黑色
+        ctx.setFontSize(22)         // 文字字号：22px
+        ctx.fillText('作者：张杰', 600 / 2, 500)
+        // 小程序码
+        const qrImgSize = 180
+        ctx.drawImage('../../images/ma.png', (600 - qrImgSize) / 2, 530, qrImgSize, qrImgSize)
+        ctx.stroke()
+        ctx.draw()
+
+       setTimeout(function(){
+         wx.canvasToTempFilePath({
+           canvasId: 'shareCanvas',
+           success: function (res) {
+             console.log(res.tempFilePath)
+             wx.saveImageToPhotosAlbum({
+               filePath: res.tempFilePath
+             })
+           }
+         })
+       },2000)
+  },
+  getUserInfo: function(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  },
+
+
+})
