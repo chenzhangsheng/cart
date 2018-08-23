9c2915d2ddcfbfa306e9f58a3bd0fa743d84521d
baishu <baishu@maihaoche.com>
2018-08-24 01:20:52 +0800

    注册页面

---
+++ pages/register/index.js
@@ -0,0 +1,37 @@
+Page({
+  data: {
+      value1: '',
+      value2: '',
+      value3: '',
+      value4: '',
+      value5: '',
+      value6: '',
+      value7: '',
+      index: 0,
+      date: '',
+  },
+
+
+  bindDateChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      date: e.detail.value
+    })
+  },
+  img_item: function (e) {
+    var that = this;
+    wx.chooseImage({
+      count: 1,
+      sizeType: ['original', 'compressed'],
+      sourceType: ['album', 'camera'],
+      success: function (res) {
+        that.setData({
+          ['tempFilePaths[' + e.target.id + ']']: res.tempFilePaths[0]
+        })
+      }
+    })
+  },
+
+
+
+});
