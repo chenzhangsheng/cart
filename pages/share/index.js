d584d4183b933496c3089ed6a50dfcbf3098ec83
baishu <baishu@maihaoche.com>
2018-09-15 15:32:39 +0800

    图片保存不成功

--- pages/share/index.js
+++ pages/share/index.js
@@ -6,18 +6,21 @@ Page({
     motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    flag: true,
   },
   //事件处理函数
-  bindViewTap: function () {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
+  // bindViewTap: function () {
+  //   wx.navigateTo({
+  //     url: '../logs/logs'
+  //   })
+  // },
   getPhoneNumber: function (e) {
     console.log(e.detail.errMsg)
     console.log(e.detail.iv)
     console.log(e.detail.encryptedData)
+    this.showMask()
+    
   },
   onLoad: function () {
     if (app.globalData.userInfo) {
@@ -51,27 +54,28 @@ Page({
   },
   onReady: function () {
 
-    // let rpx;
-    // //获取屏幕宽度，获取自适应单位
-    // wx.getSystemInfo({
-    //   success: function(res) {
-    //     rpx = res.windowWidth/375;
-    //   },
-    // })
-    // console.log(rpx,"test1 onReady");
-    // const ctx = wx.createCanvasContext('shareCanvas')
-    // // 底图
+    let rpx;
+    //获取屏幕宽度，获取自适应单位
+    wx.getSystemInfo({
+      success: function(res) {
+        rpx = res.windowWidth/375;
+      },
+    })
+    const ctx = wx.createCanvasContext('shareCanvas')
+    // 小程序码
+    const qrImgSize = 110
+    // 底图
     // ctx.drawImage('../../images/backg.png', 0, 0, 380*rpx, 600*rpx)
-    // // 作者名称
-    // // ctx.setTextAlign('center')    // 文字居中
-    // // ctx.setFillStyle('#000000')  // 文字颜色：黑色
-    // // ctx.setFontSize(22)         // 文字字号：22px
-    // // ctx.fillText('作者：张杰', 100 / 2, 200)
-    // // 小程序码
-    // const qrImgSize = 158
+    ctx.drawImage('../../images/backg.png', 0, 0, 320*rpx, 590*rpx)
+    // 作者名称
+    ctx.setTextAlign('center')    // 文字居中
+    ctx.setFillStyle('#000000')  // 文字颜色：黑色
+    ctx.setFontSize(10)         // 文字字号：22px
+    ctx.fillText('长按识别二维码', (430 - qrImgSize)*rpx / 2, 500*rpx)
     // ctx.drawImage('../../images/ma.png', (380 - qrImgSize)*rpx / 2, 400*rpx, qrImgSize*rpx, qrImgSize*rpx)
-    // ctx.stroke()
-    // ctx.draw()
+    ctx.drawImage('../../images/ma.png', (320 - qrImgSize)*rpx / 2, 380*rpx, qrImgSize*rpx, qrImgSize*rpx)
+    ctx.stroke()
+    ctx.draw()
 
     // setTimeout(function () {
     //   wx.canvasToTempFilePath({
@@ -86,13 +90,25 @@ Page({
     // }, 2000)
   },
   getUserInfo: function (e) {
-
     app.globalData.userInfo = e.detail.userInfo
     this.setData({
       userInfo: e.detail.userInfo,
       hasUserInfo: true
     })
   },
-
-
+  showMask:function(){
+    this.setData({ flag: false })
+    wx.canvasToTempFilePath({
+      canvasId: 'shareCanvas',
+      success: function (res) {
+        console.log(res.tempFilePath,'保存到相册')
+        wx.saveImageToPhotosAlbum({
+          filePath: res.tempFilePath
+        })
+      }
+    })
+  },
+  closeMask: function () {
+    this.setData({ flag: true })
+  },
 })
